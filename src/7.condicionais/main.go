c89cd2f7c2e5f7ba0a91f5cbf7c14f5658cfc64c
Lucas <lucassrod@gmail.com>
2020-09-09 19:57:09 -0300

    Switch case

--- src/7.condicionais/main.go
+++ src/7.condicionais/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func condicionalIfElseIf() {
 
@@ -36,8 +39,59 @@ func condicionalBooleanoEInicializacao() {
 	}
 }
 
+func condicionalSwitchExemploUm(valor string) {
+
+	switch valor {
+	case "Foo":
+		fmt.Println("O valor é Foo")
+	case "Bar":
+		fmt.Println("O valor é Bar")
+	case "Bob":
+		fmt.Println("O vaor é Bob")
+	default:
+		fmt.Println("Valor inválido")
+	}
+}
+
+func condicionalSwitchExemploDois() {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Bom dia")
+	case t.Hour() < 18:
+		fmt.Println("Boa tarde")
+	default:
+		fmt.Println("Boa noite")
+	}
+}
+
+// Recebe qualquer tipo
+func condicionalSwitchExemploTres(i interface{}) string {
+	switch i.(type) {
+	case int:
+		return "Inteiro"
+	case float32, float64:
+		return "Real"
+	case string:
+		return "String"
+	case func():
+		return "Função"
+	default:
+		return "Não sei"
+	}
+}
+
 func main() {
 	condicionalIfElseIf()
 	condicionalBooleano()
 	condicionalBooleanoEInicializacao()
+	condicionalSwitchExemploUm("Foo")
+	condicionalSwitchExemploUm("Bar")
+	condicionalSwitchExemploUm("Teste")
+	condicionalSwitchExemploDois()
+	fmt.Println(condicionalSwitchExemploTres(2.3))
+	fmt.Println(condicionalSwitchExemploTres(1))
+	fmt.Println(condicionalSwitchExemploTres("Opa"))
+	fmt.Println(condicionalSwitchExemploTres(func() {}))
+	fmt.Println(condicionalSwitchExemploTres(time.Now()))
 }
